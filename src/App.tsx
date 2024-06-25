544254195235867457afe2883545d309293c5389
Luis2022cr <joseluiscrz129@gmail.com>
2024-06-24 22:12:52 -0600

    update Fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 
-import CharacterList from "./components/CharacterList";
 import Main from "./components/Main";
 
 export default function App() {
   return (
     <>
         <Main/>
-        <CharacterList />
     </>
   )
 }
