b65c5326e6c31549d42de1e70c8731e1b18dd241
AbigailCalderon23 <159484003+AbigailCalderon23@users.noreply.github.com>
2024-08-05 12:09:41 -0600

    pagina de certificados  (#27)
    * pagina Mis Certificados
    
    * Modificacion Pagina C
    
    * Pagina certificados

--- src/components/paginas/MisCertificados.tsx
+++ src/components/paginas/MisCertificados.tsx
@@ -1,100 +1,145 @@
-import React, { useState } from 'react';
+import { useEffect, useState } from "react";
+import FiltroMA from "../../components/filtroMisActividades";
+import Pagination from "../../components/Pagination";
+import { NavLink } from "react-router-dom";
 
-const MisCertificados = () => {
-  const [nombre, setNombre] = useState('');
- 
-  const [inicio, setInicio] = useState('');
-  const [fechaFinalizacion, setFechaFinalizacion] = useState('');
- 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+export default function PaginaMisCertificados() {
+    useEffect(() => {
+        // titulo de la pestaña del navegador
+        document.title = "Mis Certificados - UNAH CUROC";
+    }, []);
 
-    e.preventDefault();
-    console.log('Solicitud enviada:', { 
-      nombre,inicio, fechaFinalizacion, 
-     
-    });
-    // Limpiar los campos después del envío
-    setNombre('');
+          
+    //tabla de datos
+    const initialData = [
+      
+      { nombre: "Charla", encargado: "Juan Perez",  ambito: "Cultural", fecha: "2024-07-12 01:14:23" },
+      { nombre: "Charla", encargado: "Juan Perez",  ambito: "Cultural", fecha: "2024-07-13 01:14:23" },
+      { nombre: "Charla ", encargado: "Juan Perez",  ambito: "Cultural", fecha: "2024-07-14 01:14:23" },
+      { nombre: "Charla", encargado: "Juan Perez",  ambito: "Cultural", fecha: "2024-07-16 01:14:23" },
+      { nombre: "Charla", encargado: "Juan Perez", ambito: "Cultural", fecha: "2024-07-21 01:14:23" },
+      { nombre: "Charla ", encargado: "Biblioteca",  ambito: "Cultural", fecha: "22024-07-30 01:14:23" },
+      { nombre: "Charla", encargado: "Juan Perez",  ambito: "Cultural", fecha: "2024-07-10 01:14:23" },
+      { nombre: "plantacion de arboles", encargado: "Biblioteca",  ambito: "Cultural", fecha: "2024-07-01 01:14:23" ,                                                                        },
+      { nombre: "Charla", encargado: "Juan Perez",  ambito: "Cultural", fecha: "2024-07-12 01:14:23" },
+      { nombre: "Charla", encargado: "Juan Perez",  ambito: "Cultural", fecha: "2024-07-12 01:14:23" },
+      { nombre: "Charla", encargado: "Juan Perez",  ambito: "Cultural", fecha: "2024-07-18 01:14:23" },
+      { nombre: "Charla", encargado: "Juan Perez",  ambito: "Cultural", fecha: "2024-07-12 01:14:23" },
+      { nombre: "Charla", encargado: "Juan Perez",  ambito: "Cultural", fecha: "2024-07-12 01:14:23" },
+      
+  ];
 
-    setInicio('');
-    setFechaFinalizacion('');
+
+
+
+
+
+    const [filtrarData, setFiltrarData] = useState(initialData); // Estado para datos filtrados
+    const [PaginaInicial, setPaginaInicial] = useState(1);
+    
+    //funcion de paginacion
+    const itemsPerPaginas = 10;
+    const TotalPaginas = Math.ceil(filtrarData.length / itemsPerPaginas); // Usar FiltrarData en lugar de initialData
+
+    const handlePaginasChange = (Paginas: number) => {
+        setPaginaInicial(Paginas);
+    };
+
+    const paginatedData = filtrarData.slice((PaginaInicial - 1) * itemsPerPaginas, PaginaInicial * itemsPerPaginas); // Usar FiltrarData en lugar de initialData
+
+    // Función para aplicar filtro
+    const aplicarFiltros = ( fechaInicio: string, ambito: string,  fechaFin: string, busqueda: string) => {
+        const fechaInicioDate = fechaInicio ? new Date(fechaInicio.split('T')[0]) : null; // Obtener solo la fecha
+        const fechaFinDate = fechaFin ? new Date(fechaFin.split('T')[0]) : null; // Obtener solo la fecha
+    
+        const filtrar = initialData.filter(item => {
+            const inicioDate = new Date(item.fecha.split(' ')[0]); // Obtener solo la fecha desde la cadena de inicio
+    
+            return (
+              (ambito === "" || item.ambito === ambito) &&
+                (!fechaInicioDate || inicioDate >= fechaInicioDate) &&
+                (!fechaFinDate || inicioDate <= fechaFinDate) &&
+                (busqueda === "" || item.nombre.toLowerCase().includes(busqueda.toLowerCase()))
+            );
+        });
+    
+        setFiltrarData(filtrar);
+        setPaginaInicial(1); // Reiniciar la página actual al aplicar filtros
+    };
+    
+//filtros
+    return (
+        <>
+            <div className="container mx-auto p-2">
+                <div className="block md:flex items-center justify-center mb-4 mt-2">
+
+                    <FiltroMA aplicarFiltros={aplicarFiltros}/>
+                </div>
+
+
+                <div className="rounded-xl">
+                    <div className="overflow-x-auto">
+                        <table className=" border-collapse block md:table min-w-full table-auto bg-white border border-gray-200">
+                            <thead className="block md:table-header-group">
+                                <tr 
+                                   className="border text-sm border-gray-500 md:border-none block md:table-row absolute -top-full md:top-auto -left-full md:left-auto md:relative bg-yellow-500 text-black">
+                                   <th className="p-2 font-bold md:border md:border-grey-500 text-left block md:table-cell">Nombre</th>
+                                   <th className="p-2 font-bold md:border md:border-grey-500 text-left block md:table-cell">Encargado</th>
+                                   <th className="p-2 font-bold md:border md:border-grey-500 text-left block md:table-cell">Ambito</th>
+                                   <th className="p-2 font-bold md:border md:border-grey-500 text-left block md:table-cell">Fecha</th>
+                                   <th className="p-2 font-bold md:border md:border-grey-500 text-left block md:table-cell">Certificado</th>
    
-  };
 
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="w-full max-w-4xl">
+                                  
+                                </tr>
+                            </thead>
+                            <tbody className="block md:table-row-group text-sm md:text-xs">
+                                {paginatedData.map((item, index) => (
+                                    <tr key={index} className="bg-yellow-500 md:bg-white text-left md:text-center hover:bg-gray-200 transition-colors duration-200 border border-gray-500 md:border-none block md:table-row">
+                                        <td className="p-1 md:border md:border-gray-500 block md:table-cell">
+                                            <span className="inline-block w-1/3 md:hidden font-bold mr-4">Nombre:</span>{item.nombre}
+                                        </td>
+                                        <td className="p-1 md:border md:border-gray-500 block md:table-cell">
+                                            <span className="inline-block w-1/3 md:hidden font-bold mr-4">Encargado:</span>{item.encargado}                                        </td>
+                                        
+                                        <td className="p-1 md:border md:border-gray-500 block md:table-cell">
+                                            <span className="inline-block w-1/3 md:hidden font-bold mr-4">Ambito:</span>{item.ambito}
+                                        </td>
+                                        <td className="p-1 md:border md:border-gray-500 block md:table-cell">
+                                            <span className="inline-block w-1/3 md:hidden font-bold mr-4">Fecha:</span>{item.fecha}
+                                        </td>
+                                        
 
-        <h1 className="text-center text-2xl font-bold mb-4">Mis Certificados VOAE</h1>
+                                        <td className="p-1 md:border text-center md:border-gray-500 block md:table-cell relative">
+                                    <NavLink
+                                        to={
+                                            location.pathname.includes('dashboard-coordinador')
+                                                ? "/dashboard-coordinador/mis-certificados/certificados-actividad"
+                                                : location.pathname.includes('dashboard-estudiante')
+                                                    ? "/dashboard-estudiante/mis-certificados/certificados"
+                                                    : "/dashboard-voae/mis-certificados/vcertificado"
+                                        }
+                                        className="flex justify-center items-center font-bold group"
+                                    >
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={32} height={32} color={"#000000"} fill={"none"} className="group-hover:text-blue-500 hidden md:block">
+                                                <path d="M21.544 11.045C21.848 11.4713 22 11.6845 22 12C22 12.3155 21.848 12.5287 21.544 12.955C20.1779 14.8706 16.6892 19 12 19C7.31078 19 3.8221 14.8706 2.45604 12.955C2.15201 12.5287 2 12.3155 2 12C2 11.6845 2.15201 11.4713 2.45604 11.045C3.8221 9.12944 7.31078 5 12 5C16.6892 5 20.1779 9.12944 21.544 11.045Z" stroke="currentColor" strokeWidth="1.5" />
+                                                <path d="M15 12C15 10.3431 13.6569 9 12 9C10.3431 9 9 10.3431 9 12C9 13.6569 10.3431 15 12 15C13.6569 15 15 13.6569 15 12Z" stroke="currentColor" strokeWidth="1.5" />
+                                            </svg>
+                                            <span className="hover:text-blue-500 group-hover:text-blue-500 md:hidden">Ver detalles</span>
+                                    </NavLink>
+                                    </td>
 
-        <div className="bg-yellow-500 shadow-lg rounded-lg flex overflow-hidden mb-4">
-          <div className="w-1/3 bg-blue-900 p-6 rounded-l-lg flex flex-col items-center">
-            <label className="block text-white text-sm font-bold mb-[2.5rem]" htmlFor="nombre">
-              Nombre de la actividad:
-            </label>
-              
-            <label className="block text-white text-sm font-bold mb-[2rem]" htmlFor="inicio">
-              Inicio:
-            </label>
-            <label className="block text-white text-sm font-bold mb-[2rem]" htmlFor="fechaFinalizacion">
-              Fecha de Finalización:
-            </label>
+                                    </tr>
+                                ))}
 
-          </div>
-          <div className="w-2/3 p-6">
-            <form onSubmit={handleSubmit} className="space-y-4" id="solicitudForm">
-              <div className="mb-5">
-                <input
-                  type="text"
-                  placeholder='Ingrese un nombre'
-                  id="nombre"
-                  value={nombre}
-                  onChange={(e) => setNombre(e.target.value)}
-                  className="shadow appearance-none border rounded-full w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  required
-                />
-              </div>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
 
-              
-              
-             
-              <div className="mb-5">
-                <input
-                  type="datetime-local"
-                  id="inicio"
-                  value={inicio}
-                  onChange={(e) => setInicio(e.target.value)}
-                  className="shadow appearance-none border rounded-full w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  required
-                />
-              </div>
-              <div className="mb-5">
-                <input
-                  type="datetime-local"
-                  id="fechaFinalizacion"
-                  value={fechaFinalizacion}
-                  onChange={(e) => setFechaFinalizacion(e.target.value)}
-                  className="shadow appearance-none border rounded-full w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  required
-                />
-              </div>
-             
-             
-              
-              
-              <button
-                type="submit"
-                className="bg-blue-900 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline"
-              >
-                Ver Cerificado
-              </button>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
+                <Pagination PaginaInicial={PaginaInicial} TotalPaginas={TotalPaginas} onPageChange={handlePaginasChange} />
 
-export default MisCertificados;
-
+            </div>
+        </>
+    )
+}
