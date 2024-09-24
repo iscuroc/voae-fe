5d4e4a59a58a27a86de7569e9a1fed2eb36d8775
erickArita <erick.emao@gmail.com>
2024-09-24 09:44:57 -0600

    PC-174: agregandoi campo para organización al confirmar cuenta

--- src/api/servicios/organizaciones.ts
+++ src/api/servicios/organizaciones.ts
@@ -1,11 +1,18 @@
-import axiosInstance from "../axiosInstance";
+import axiosInstance, { useAxios } from "../axiosInstance";
 
-export interface organizations{
-    id: number;
-    name: string
+export interface organizations {
+  id: number;
+  name: string;
 }
 
 export const obtenerLasOrganizaciones = async (): Promise<organizations[]> => {
-    const response = await axiosInstance.get('/organizations');
-    return response.data;
+  const response = await axiosInstance.get("/organizations");
+  return response.data;
+};
+
+export const useGetOrganizationsQuery = () => {
+  const response = useAxios<organizations[]>({
+    url: `/organizations`,
+  });
+  return response;
 };
