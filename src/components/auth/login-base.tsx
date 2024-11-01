9d6e8a77f999cf9ad81ddf07c9f782e7cdd80314
erickArita <erick.emao@gmail.com>
2024-10-31 18:52:08 -0600

    PC-211: componente reutilizable para formularios de auh

--- src/components/auth/login-base.tsx
+++ src/components/auth/login-base.tsx
@@ -5,7 +5,7 @@ import logo2 from "@/assets/logo2.avif";
 
 export const LoginBase: FC<PropsWithChildren> = ({ children }) => {
   return (
-    <div className="flex flex-wrap flex-1 justify-center gap-5">
+    <div className="flex  flex-wrap flex-1 justify-center gap-5 ">
       <div className="flex items-center justify-center">
         <img
           src={logo1}
@@ -15,7 +15,9 @@ export const LoginBase: FC<PropsWithChildren> = ({ children }) => {
 
         <img src={logo2} alt="Logo 2" className="w-40 h-32 md:w-80 md:h-52" />
       </div>
-      <div className="shadow-lg rounded-lg ">{children}</div>
+      <div className="shadow-lg rounded-lg shadow-gray-300 flex items-center ">
+        {children}
+      </div>
     </div>
   );
 };
