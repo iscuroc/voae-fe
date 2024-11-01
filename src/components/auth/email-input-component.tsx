2fc5e5a0466d32e46bbfa398f3016608704c010b
Erick Marley Arita <58413651+erickArita@users.noreply.github.com>
2024-11-01 10:47:45 -0600

    Pc 211 (#54)
    * Refactor email and password input components for login page
    coomon auth view layout
    
    * PC-211: manejando errores
    
    * PC-211: componente reutilizable para formularios de auh
    
    * PC-211:refactoir de vista para registrarse
    
    * PC-211: manejo de errores y actions http
    
    * PC-211: removiendo espacio bajo el header
    
    * PC-211: componente reutilizable de contraseña
    
    * PC-211: refactor a layout general mientras no se esta autenticado
    
    * PC-211: mejor manejo de errores y titulo al formulario de login
    
    * PC-211: refactor y mejoras de ui a la vista de olvidé mi contraseña
    
    * PC-211:
     - mejoras de ui
     -  refactoir
     - mejor  manejo de errores
    en vista de registro de usuario
    
    * PC-211: paquete para hooks comunes
    
    * PC-211: improve reset password flow and enhance error handling
    
    * remove console log from error handling in reset password component
    
    * refactor login component to navigate to dashboard on successful login and clean up unused imports
    
    * PC-211: redirigir a las actividades en curso al ahcer login
    
    * PC-211: fix validaci[on de correo no estaba incluyendo el correo de los maestros

--- src/components/auth/email-input-component.tsx
+++ src/components/auth/email-input-component.tsx
@@ -31,7 +31,7 @@ export const EmailInput: FC<Partial<EmailInputProps>> = (props) => (
       },
       //el correo dbe ser de la unah unah.hn o unah.edu.hn o parecidos
       {
-        pattern: /^[\w-\.]+@unah\.(edu|hn)$/,
+        pattern: /^[\w-\.]+@(unah\.edu\.hn|unah\.hn)$/,
         message:
           "Por favor ingrese un correo de la UNAH @unah.hn o @unah.edu.hn",
       },
