74829d73b8f3190b53300d63bc7a7656aef981be
erickArita <erick.emao@gmail.com>
2024-11-30 14:55:29 -0600

    Add: mis actividades terminado

--- src/components/filtros/filtroMisActividades.tsx
+++ src/components/filtros/filtroMisActividades.tsx
@@ -1,143 +1,73 @@
-import React, { useState } from "react";
+import { LightFilter, ProForm, ProFormDatePicker, ProFormSelect, ProFormText } from "@ant-design/pro-components";
+import { useForm } from "antd/es/form/Form";
 
 interface FiltroProps {
-    aplicarFiltros: (ambito: string, fechaInicio: string, fechaFin: string, busqueda: string) => void;
+  aplicarFiltros: (ambito: string, fechaInicio: string, fechaFin: string, busqueda: string) => void;
 }
 
 const FiltroMA: React.FC<FiltroProps> = ({ aplicarFiltros }) => {
-    const [fechaInicio, setFechaInicio] = useState<string>("");
-    const [fechaFin, setFechaFin] = useState<string>("");
-    const [busqueda, setBusqueda] = useState<string>("");
-    const [ambito, setAmbito] = useState<string>("");
-    const [showResetButton, setShowResetButton] = useState<boolean>(false);
-
-    const handleFechaInicioChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const inputValue = event.target.value;
-        setFechaInicio(inputValue);
-    };
-
-    const handleFechaFinChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const inputValue = event.target.value;
-        setFechaFin(inputValue);
-    };
-
-    const handleBusquedaChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setBusqueda(event.target.value);
-    };
-
-    const handleAmbitoChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-        setAmbito(event.target.value);
-    };
-
-    const handleFiltrarClick = () => {
+  const [ form ] = useForm();
+  return (
+    <LightFilter
+      form={form}
+      layout="horizontal"
+      submitter={{
+        render: (_, dom) => <div className="flex justify-center w-full gap-4">{dom}</div>,
+        searchConfig: {
+          resetText: 'Resetear',
+          submitText: 'Filtrar',
+        },
+      }}
+      onFieldsChange={(changedFields: _, allFields) => {
+        const ambito = allFields.find(field => field.name[ 0 ] === 'ambito')?.value;
+        const fechaInicio = allFields.find(field => field.name[ 0 ] === 'fechaInicio')?.value;
+        const fechaFin = allFields.find(field => field.name[ 0 ] === 'fechaFin')?.value;
+        const busqueda = allFields.find(field => field.name[ 0 ] === 'busqueda')?.value;
         aplicarFiltros(ambito, fechaInicio, fechaFin, busqueda);
-        setShowResetButton(true);
-    };
+      }}
+    >
+      <ProForm.Group>
+        <ProFormDatePicker
+          name="fechaInicio"
+          label="Fecha Inicio"
+          placeholder="DD-MM-AAAA"
+          fieldProps={{
+            format: {
+              format: 'DD-MM-YYYY',
+              type: 'mask',
+            },
+          }}
+        />
+        <ProFormDatePicker
+          name="fechaFin"
+          label="Fecha Fin"
+          placeholder="DD-MM-AAAA"
 
-    const resetFilters = () => {
-        setFechaInicio("");
-        setFechaFin("");
-        setBusqueda("");
-        setAmbito("");
-        aplicarFiltros("", "", "", "");
-        setShowResetButton(false);
-    };
+        />
+      </ProForm.Group>
+      <ProFormSelect
+        name="ambito"
+        label="Ámbito"
+        options={[
+          { label: 'Seleccione Ámbito...', value: '' },
+          { label: 'Académico', value: 'Academico' },
+          { label: 'Cultural', value: 'Cultural' },
+          { label: 'Social', value: 'Social' },
+          { label: 'Deportivo', value: 'Deportivo' },
+        ]}
 
-    const areFiltersApplied = fechaInicio || fechaFin || busqueda || ambito;
+      />
+      <ProFormText
+        name="busqueda"
+        label="Buscar"
+        placeholder="Nombre de actividad"
 
-    return (
-        <>
-
-<div className=" flex flex-wrap justify-center w-full gap-4">
-    <div className="w-full md:w-auto">
-        <label className="block text-sm font-medium">Fecha</label>
-        <div className="block md:flex md:space-x-2">
-            <input
-                className="h-10 px-3 py-2 text-sm w-full rounded-xl border border-input border-black bg-white focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-                value={fechaInicio}
-                onChange={handleFechaInicioChange}
-                placeholder="DD-MM-AAAA"
-                type="date"
-            />
-            <div className="text-center justify-center mt-1 hidden md:flex">-</div>
-            <input
-                className="h-10 px-3 py-2 text-sm w-full mt-5 md:mt-0 rounded-xl border border-input border-black bg-white focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-                value={fechaFin}
-                onChange={handleFechaFinChange}
-                placeholder="DD-MM-AAAA"
-                type="date"
-            />
-        </div>
-    </div>
-    <div className="w-full md:w-auto">
-        <label className="block text-sm font-medium">Ámbito</label>
-        <select
-            className="h-10 w-full md:w-48 rounded-xl border border-input border-black bg-background px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
-            value={ambito}
-            onChange={handleAmbitoChange}
-        >
-            <option value="">Seleccione Ámbito...</option>
-            <option value="Academico">Académico</option>
-            <option value="Cultural">Cultural</option>
-            <option value="Social">Social</option>
-            <option value="Deportivo">Deportivo</option>
-        </select>
-    </div>
-    <div className="w-full md:w-auto">
-        <label className="block text-sm font-medium">Buscar</label>
-        <div className="flex items-center">
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                width="32"
-                height="32"
-                className="mr-2 text-gray-400"
-                fill="none"
-            >
-                <path
-                    d="M17.5 17.5L22 22"
-                    stroke="currentColor"
-                    strokeWidth="1.5"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                />
-                <path
-                    d="M20 11C20 6.02944 15.9706 2 11 2C6.02944 2 2 6.02944 2 11C2 15.9706 6.02944 20 11 20C15.9706 20 20 15.9706 20 11Z"
-                    stroke="currentColor"
-                    strokeWidth="1.5"
-                    strokeLinejoin="round"
-                />
-            </svg>
-            <input
-                className="h-10 w-full rounded-xl border border-black border-input bg-background px-3 py-2 text-sm"
-                placeholder="Nombre de actividad"
-                type="search"
-                value={busqueda}
-                onChange={handleBusquedaChange}
-            />
-        </div>
-    </div>
-    <div className="w-full md:w-auto flex items-end">
-        <button
-            className="h-10 px-6 py-2 text-sm mt-5 md:mt-0 font-medium text-white bg-blue-900 rounded-xl hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 mr-2"
-            onClick={handleFiltrarClick}
-        >
-            Filtrar
-        </button>
-        {showResetButton && (
-            <button
-                className={`h-10 px-6 py-2 text-sm font-medium text-white rounded-xl focus:outline-none focus:ring-2 focus:ring-offset-2 ${areFiltersApplied ? "bg-red-500 hover:bg-red-400 focus:ring-red-500" : "bg-gray-400 cursor-not-allowed"}`}
-                onClick={resetFilters}
-                disabled={!areFiltersApplied}
-            >
-                Resetear
-            </button>
-        )}
-    </div>
-</div>
-
-        </>
-    );
+        lightProps={{
+          light: false,
+        }}
+      />
+    </LightFilter>
+  );
 }
 
 export default FiltroMA;
--- src/pages/estudiantes_dashboard/PaginaMisActividades.tsx
+++ src/pages/estudiantes_dashboard/PaginaMisActividades.tsx
@@ -1,171 +1,167 @@
 
-import { useEffect, useState } from "react";
-import Pagination from "../../components/Pagination";
-import FiltroMA from "../../components/filtros/filtroMisActividades";
-import { estadoActividadMap, EtiquetasAmbitosActividad, formatDate } from "@/api/servicios/enums";
-import Skeleton from "@/components/Skeleton";
-import { MisActividades } from "@/api/servicios/actividades";
 import axiosInstance from "@/api/axiosInstance";
+import type { MisActividades } from "@/api/servicios/actividades";
+import { EtiquetasAmbitosActividad, estadoActividadMap, formatDate } from "@/api/servicios/enums";
+import Skeleton from "@/components/Skeleton";
+import { type ProColumns, ProDescriptions, ProTable } from "@ant-design/pro-components";
+import { useEffect, useMemo, useState } from "react";
+import FiltroMA from "../../components/filtros/filtroMisActividades";
+
 export default function PaginaMisActividades() {
-    useEffect(() => {
-        document.title = "Mis Horas - UNAH CUROC";
-    }, []);
+  useEffect(() => {
+    document.title = "Mis Horas - UNAH CUROC";
+  }, []);
 
-    const [filtrarData, setFiltrarData] = useState<MisActividades[]>([]);
-    const [loading, setLoading] = useState<boolean>(true);
-    const [error, setError] = useState<string | null>(null);
-    const [paginaInicial, setPaginaInicial] = useState(1);
+  const [ filtrarData, setFiltrarData ] = useState<MisActividades[]>([]);
+  const [ loading, setLoading ] = useState<boolean>(true);
+  const [ error, setError ] = useState<string | null>(null);
+  const [ paginaInicial, setPaginaInicial ] = useState(1);
+  const [ searchData, setSearchData ] = useState<MisActividades[]>([]);
 
-    useEffect(() => {
-        const obtenerDatos = async () => {
-            setLoading(true);
-            try {
-                const response = await axiosInstance.get('/users/my-activities', {});
-                 setFiltrarData(response.data);
-            } catch (error) {
-                setError('Hay problemas de conexion con el Servidor');
-            } finally {
-                setLoading(false);
-            }
-        };
-        obtenerDatos();
-    }, []);
-
-    const itemsPerPaginas = 10;
-    const TotalPaginas = Math.ceil(filtrarData.length / itemsPerPaginas);
-
-    const handlePaginasChange = (Paginas: number) => {
-        setPaginaInicial(Paginas);
+  useEffect(() => {
+    const obtenerDatos = async () => {
+      setLoading(true);
+      try {
+        const response = await axiosInstance.get('/users/my-activities', {});
+        setFiltrarData(response.data);
+        setSearchData(response.data);
+      } catch(error) {
+        setError('Hay problemas de conexion con el Servidor');
+      } finally {
+        setLoading(false);
+      }
     };
+    obtenerDatos();
+  }, []);
 
-    const paginatedData = filtrarData.slice((paginaInicial - 1) * itemsPerPaginas, paginaInicial * itemsPerPaginas);
+  const itemsPerPaginas = 10;
 
-    const aplicarFiltros = (ambito: string, fechaInicio: string, fechaFin: string, busqueda: string) => {
-        const fechaInicioDate = fechaInicio ? new Date(fechaInicio.split('T')[0]) : null;
-        const fechaFinDate = fechaFin ? new Date(fechaFin.split('T')[0]) : null;
+  const handlePaginasChange = (Paginas: number) => {
+    setPaginaInicial(Paginas);
+  };
 
-        const filtrar = filtrarData.filter(item => {
-            const inicioDate = new Date(item.startDate.split(' ')[0]);
+  const paginatedData = searchData.slice((paginaInicial - 1) * itemsPerPaginas, paginaInicial * itemsPerPaginas);
+  const aplicarFiltros = (ambito: string, fechaInicio: string, fechaFin: string, busqueda: string) => {
+    console.log(ambito, fechaInicio, fechaFin, busqueda);
 
-            return (
-                (ambito === "" || item.activityScopes.some(s => EtiquetasAmbitosActividad[s.scope] === ambito)) &&
-                (!fechaInicioDate || inicioDate >= fechaInicioDate) &&
-                (!fechaFinDate || inicioDate <= fechaFinDate) &&
-                (busqueda === "" || item.name.toLowerCase().includes(busqueda.toLowerCase()))
-            );
-        });
+    const fechaInicioDate = fechaInicio ? new Date(fechaInicio.split('T')[ 0 ]) : null;
+    const fechaFinDate = fechaFin ? new Date(fechaFin.split('T')[ 0 ]) : null;
 
-        setFiltrarData(filtrar);
-        setPaginaInicial(1);
-    };
+    const filtrar = filtrarData.filter(item => {
+      const inicioDate = new Date(item.startDate.split(' ')[ 0 ]);
 
-    if (loading) {
-        return <Skeleton />;
-    }
+      const cumpleAmbito = ambito === "" || item.activityScopes.some(s => EtiquetasAmbitosActividad[ s.scope ] === ambito);
+      const cumpleFechaInicio = !fechaInicioDate || inicioDate >= fechaInicioDate;
+      const cumpleFechaFin = !fechaFinDate || inicioDate <= fechaFinDate;
+      const cumpleBusqueda = item.name.toLowerCase().includes(busqueda?.toLowerCase());
 
-    if (error) {
-        return <div className="text-2xl font-bold text-center mt-20">Error: {error}</div>;
-    }
-
-    // Obtener todos los ámbitos y las horas para generar las columnas y filas
-    const ambitosHoras: { [ambito: string]: number } = {};
-
-    filtrarData.forEach(actividad => {
-        actividad.memberScopes.forEach(memberScopes => {
-            const ambitoNombre = EtiquetasAmbitosActividad[memberScopes.scope] || `Ámbito ${memberScopes.scope}`;
-            if (!ambitosHoras[ambitoNombre]) {
-                ambitosHoras[ambitoNombre] = 0;
-            }
-            ambitosHoras[ambitoNombre] += memberScopes.hours;
-        });
+      return cumpleAmbito && cumpleFechaInicio && cumpleFechaFin && cumpleBusqueda;
     });
 
-    const ambitos = Object.keys(ambitosHoras);
-    const horas = Object.values(ambitosHoras);
+    setSearchData(filtrar);
+    setPaginaInicial(1);
+  };
+  // Obtener todos los ámbitos y las horas para generar las columnas y filas
 
-    return (
-        <>
-            <div className="container mx-auto p-2">
-                <div className="block md:flex items-center justify-center mb-4 mt-2">
-                    <FiltroMA aplicarFiltros={aplicarFiltros} />
-                </div>
+  const ambitosHorasMapped = useMemo(() => {
+    const ambitosHoras: { [ ambito: string ]: number } = {};
+    // biome-ignore lint/complexity/noForEach: <explanation>
+    filtrarData.forEach(actividad => {
+      // biome-ignore lint/complexity/noForEach: <explanation>
+      actividad.memberScopes.forEach(memberScopes => {
+        const ambitoNombre = EtiquetasAmbitosActividad[ memberScopes.scope ] || `Ámbito ${memberScopes.scope}`;
+        if(!ambitosHoras[ ambitoNombre ]) {
+          ambitosHoras[ ambitoNombre ] = 0;
+        }
+        ambitosHoras[ ambitoNombre ] += memberScopes.hours;
+      });
+    });
 
-                <h2 className="text-center my-5 text-xl font-bold underline">Horas</h2>
-                <div className="bg-white shadow-lg rounded-lg overflow-hidden">
-                    <div className="overflow-x-auto">
-                    <table className="min-w-full table-auto bg-white border border-gray-200">
-    <thead className="hidden md:table-header-group">
-        <tr className="bg-blue-900 text-white">
-            <th className="p-4 font-bold text-left">Ámbito</th>
-            <th className="p-4 font-bold text-left">Horas Totales</th>
-        </tr>
-    </thead>
-    <tbody>
-        {/* Para pantallas pequeñas */}
-        {ambitos.map((ambito, index) => (
-            <tr key={index} className="md:hidden text-white">
-                <td className="p-4 bg-blue-900 font-bold text-left">{ambito}</td>
-                <td className="p-4 text-black border-b border-gray-200">{horas[index]}</td>
-            </tr>
-        ))}
+    return ambitosHoras;
 
-        {/* Para pantallas más grandes */}
-        {ambitos.map((ambito, index) => (
-            <tr key={index} className="hidden md:table-row bg-white">
-                <td className="p-4 font-bold text-left">{ambito}</td>
-                <td className="p-4 border-b border-gray-200">{horas[index]}</td>
-            </tr>
-        ))}
-    </tbody>
-</table>
+  }, [ filtrarData ]);
 
-                    </div>
-                </div>
+  if(loading) {
+    return <Skeleton />;
+  }
 
-                <h2 className="text-center my-5 text-xl font-bold underline">Actividades</h2>
-                <div className="rounded-xl">
-                    <div className="overflow-x-auto">
-                        <table className="border-collapse block md:table min-w-full table-auto bg-white border border-gray-200">
-                            <thead className="block md:table-header-group">
-                                <tr className="border text-sm border-gray-500 md:border-none block md:table-row absolute -top-full md:top-auto -left-full md:left-auto md:relative bg-yellow-500 text-black">
-                                    <th className="p-2 font-bold md:border md:border-grey-500 text-left block md:table-cell">Nombre</th>
-                                    <th className="p-2 font-bold md:border md:border-grey-500 text-left block md:table-cell">Descripcion</th>
-                                    <th className="p-2 font-bold md:border md:border-grey-500 text-left block md:table-cell">Ámbitos</th>
-                                    <th className="p-2 font-bold md:border md:border-grey-500 text-left block md:table-cell">Fecha Inicio</th>
-                                    <th className="p-2 font-bold md:border md:border-grey-500 text-left block md:table-cell">Fecha Final</th>
-                                    <th className="p-2 font-bold md:border md:border-grey-500 text-left block md:table-cell">Estado</th>
-                                </tr>
-                            </thead>
-                            <tbody className="block md:table-row-group text-sm md:text-xs">
-                                {paginatedData.map((item, index) => (
-                                    <tr key={index} className="bg-yellow-500 md:bg-white text-left md:text-center hover:bg-gray-200 transition-colors duration-200 border border-gray-500 md:border-none block md:table-row">
-                                        <td className="p-1 md:border md:border-gray-500 block md:table-cell">
-                                            <span className="inline-block w-1/3 md:hidden font-bold mr-4">Nombre:</span>{item.name}
-                                        </td>
-                                        <td className="p-1 md:border md:border-gray-500 block md:table-cell">
-                                            <span className="inline-block w-1/3 md:hidden font-bold mr-4">Descripcion:</span>{item.description}
-                                        </td>
-                                        <td className="p-1 md:border md:border-gray-500 block md:table-cell">
-                                            <span className="inline-block w-1/3 md:hidden font-bold mr-4">Ámbitos:</span>{item.activityScopes.map(s => EtiquetasAmbitosActividad[s.scope] || s.scope).join(", ")}
-                                        </td>
-                                        <td className="p-1 md:border md:border-gray-500 block md:table-cell">
-                                            <span className="inline-block w-1/3 md:hidden font-bold mr-4">Fecha Inicio:</span>{formatDate(item.startDate)}
-                                        </td>
-                                        <td className="p-1 md:border md:border-gray-500 block md:table-cell">
-                                            <span className="inline-block w-1/3 md:hidden font-bold mr-4">Fecha final:</span>{formatDate(item.endDate)}
-                                        </td>
-                                        <td className="p-1 md:border md:border-gray-500 block md:table-cell">
-                                            <span className="inline-block w-1/3 md:hidden font-bold mr-4">Estado:</span>{estadoActividadMap[item.activityStatus] || item.activityStatus}
-                                        </td>
-                                    </tr>
-                                ))}
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
+  if(error) {
+    return <div className="text-2xl font-bold text-center mt-20">Error: {error}</div>;
+  }
 
-                <Pagination PaginaInicial={paginaInicial} TotalPaginas={TotalPaginas} onPageChange={handlePaginasChange} />
-            </div>
-        </>
-    );
+  const ambitos = Object.keys(ambitosHorasMapped);
+
+  const columns: ProColumns<MisActividades>[] = [
+    {
+      title: 'Nombre',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'Descripcion',
+      dataIndex: 'description',
+      key: 'description',
+      ellipsis: true,
+    },
+    {
+      title: 'Ámbitos',
+      dataIndex: 'activityScopes',
+      key: 'activityScopes',
+      render: (_, record) => record.activityScopes.map(s => EtiquetasAmbitosActividad[ s.scope ] || s.scope).join(", "),
+    },
+    {
+      title: 'Fecha Inicio',
+      dataIndex: 'startDate',
+      key: 'startDate',
+      render: (_, record) => formatDate(record.startDate),
+      width: 150,
+    },
+    {
+      title: 'Fecha Final',
+      dataIndex: 'endDate',
+      key: 'endDate',
+      width: 150,
+      render: (_, record) => formatDate(record.endDate),
+    },
+    {
+      title: 'Estado',
+      dataIndex: 'activityStatus',
+      key: 'activityStatus',
+      render: (_, record) => estadoActividadMap[ record.activityStatus ] || record.activityStatus,
+    },
+  ];
+
+  return (
+    <>
+      <div className="container mx-auto p-2">
+
+        <ProDescriptions
+          title={<h2>Total de horas</h2>}
+          dataSource={ambitosHorasMapped}
+          columns={ambitos.map(ambito => ({
+            title: ambito,
+            dataIndex: ambito,
+            key: ambito,
+          }))}
+        />
+        <ProTable<MisActividades>
+          headerTitle={<h2 className="text-center my-5 text-md font-bold">Actividades</h2>}
+          toolbar={{ filter: (<FiltroMA aplicarFiltros={aplicarFiltros} />) }}
+          ghost
+          columns={columns}
+          dataSource={paginatedData}
+          rowKey="id"
+          pagination={{
+            current: paginaInicial,
+            pageSize: itemsPerPaginas,
+            total: filtrarData.length,
+            onChange: handlePaginasChange,
+          }}
+          scroll={{ x: 768 }}
+          search={false}
+          options={false}
+        />
+      </div>
+    </>
+  );
 }
